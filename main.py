a0e0c0a7cb47307572305b0dd5f4fcd7b8e2f7f9
erikajacobs <ErikaAshley3@gmail.com>
2020-08-02 19:27:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,51 @@ for i in range(len(getTable)):
 final_records = {}
     
 for i in range(0, len(getTable), 6):
-    # Add to Final Records
-    pass
+    recordDict = {}
+    recordDict['seasons_num'] = getTable[i]
+    recordDict['network'] = getTable[i+2]
+    recordDict['og_air_dt'] = getTable[i+3]
+    recordDict['last_air_dt'] = getTable[i+4]
+    recordDict['episodes_num'] = getTable[i+5]
+     
+    #Key in Final Dictionary - Show Name
+    final_records[getTable[i+1]] = recordDict
+    
+#%%
+
+show_list = list(final_records.keys())
+show_list.sort()
+show_list_dup = [i for i in show_list if 'Law & Order' in i or 'CSI' in i or 'NCIS' in i]
+show_list_main = [i for i in show_list if i not in show_list_dup]
+
+
+issues = []
+episodeDict = {}
+
+import requests
+
+for i in show_list_main:
+    if 'Law & Order' in i or 'CSI' in i or 'NCIS' in i:
+        pass
+    else:
+        showname = i.replace(" ", '%20')
+        page = f'http://api.tvmaze.com/singlesearch/shows?q={showname}&embed=episodes'
+        
+        try:
+            source = requests.get(page).json()
+            episodeDict[showname] = requests.get(page).json()
+            
+        except:
+            issues.append(i)
+        
+        
+print(issues)
+
+# NSI, CSI, Law & Order
+
+
+
+
 
 #%%
 
@@ -39,7 +82,6 @@ for i in range(0, len(getTable), 6):
 }
 
 '''
-
 
 
 # Sources
