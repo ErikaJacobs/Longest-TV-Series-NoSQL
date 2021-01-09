6200e30b905a69b6e760a1a40f3d40e590aacb44
erikajacobs <ErikaAshley3@gmail.com>
2021-01-08 19:58:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Get Longest Running Shows from Wikipedia
 
 import requests
+import boto3
+import time
 
 page_source = 'https://en.wikipedia.org/wiki/List_of_longest-running_scripted_U.S._primetime_television_series'
 source = requests.get(page_source).text
@@ -14,10 +16,10 @@ getTable = [i for i in getTable if "Series shaded" not in str(i)]
 
 for i in range(len(getTable)):
     getTable[i] = getTable[i].get_text().replace('\n','')
-    
-    if '[' in getTable[i]:
-        pos = getTable[i].find('[')
-        getTable[i] = getTable[i][0:pos]
+    while '[' in getTable[i]:
+        start = getTable[i].find( '[' )
+        end = getTable[i].find( ']' )
+        getTable[i] = getTable[i][0:start] + getTable[i][end+1:]
 
 final_records = {}
     
@@ -28,63 +30,101 @@ for i in range(0, len(getTable), 6):
     recordDict['og_air_dt'] = getTable[i+3]
     recordDict['last_air_dt'] = getTable[i+4]
     recordDict['episodes_num'] = getTable[i+5]
+    recordDict['show_name'] = getTable[i+1]
      
     #Key in Final Dictionary - Show Name
     final_records[getTable[i+1]] = recordDict
-    
+
 #%%
 
-show_list = list(final_records.keys())
-show_list.sort()
-show_list_dup = [i for i in show_list if 'Law & Order' in i or 'CSI' in i or 'NCIS' in i]
-show_list_old = [i for i in show_list if 'Fireside Theatre' in i or 'Lux Video Theatre' in i or 'The Loretta Young Show' in i]
-show_list_main = [i for i in show_list if i not in show_list_dup and i not in show_list_old]
-
-
+# Pull Episode Information from API
 issues = []
 episodeDict = {}
 
-import requests
-
-for i in show_list_main:
-    if 'Law & Order' in i or 'CSI' in i or 'NCIS' in i:
-        pass
+for i in final_records.keys():
+    if i in ['Law & Order', 'CSI', 'NCIS']:
+        showname = i.replace('& ','').replace(': ', '-').replace(" ", '-').lower()
     else:
         showname = i.replace(" ", '%20')
-        page = f'http://api.tvmaze.com/singlesearch/shows?q={showname}&embed=episodes'
+
+    page = f'http://api.tvmaze.com/singlesearch/shows?q={showname}&embed=episodes'
+    
+    try:
+        source = requests.get(page).json()
+        episodeDict[i] = source['_embedded']['episodes']
         
-        try:
-            source = requests.get(page).json()
-            episodeDict[showname] = requests.get(page).json()
-            
-        except:
-            issues.append(i)
-
-# NSI, CSI, Law & Order
-
-
-
-
-
+    except:
+        issues.append(i)
+        
+# Remove Issue Shows From final_records - No episode Info
+for show in issues:
+    del final_records[show]
+    
 #%%
 
-# Example Record
+# Creates Dictionary of show's seasons and episodes
+for show in episodeDict.keys():
+    showDict = {}
+    
+    for episode in episodeDict[show]:
+        epDict = {}
+        season = f"s{episode['season']}"
+        ep = f"ep{episode['number']}"
+        epDict['airdate'] = episode['airdate']
+        epDict['name'] = episode['name']
+        epDict['summary'] = str(episode['summary'])
+        
+        while '<' in epDict['summary']:
+            start = epDict['summary'].find( '<' )
+            end = epDict['summary'].find( '>' )
+            epDict['summary'] = epDict['summary'][0:start] + epDict['summary'][end+1:]
+         
+        if season not in showDict.keys():
+            showDict[season] = {}
+        else:
+            pass
+        
+        showDict[season][ep] = epDict
+        final_records[show]['episodes'] = showDict
+        
+#%%
 
-'''
-'The Simpsons':{
-'network': 'FOX',
-'seasons_num': '31',
-'episodes_num': '684',
-'og_air_dt': '1989-12-17',
-'season info': {
-    s1_eps: [], 
-    s2_eps: [],
-    etc.
+# Creates table in DynamoDB and uploads data    
+dynamodb = boto3.resource('dynamodb')
+dynamodb_client = boto3.client('dynamodb')
+
+table = dynamodb.create_table(
+    TableName='tv_shows',
+    KeySchema=[
+        {
+            'AttributeName': 'show_name',
+            'KeyType': 'HASH'
+        },
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'show_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 11,
+        'WriteCapacityUnits': 11,
     }
-}
+)
 
-'''
-
+# Give the DynamoDB table time to create
+time.sleep(45)
+    
+try:
+    for show in final_records:
+        with table.batch_writer() as batch:
+            batch.put_item(
+                Item=final_records[show])  
+except dynamodb_client.exceptions.ResourceInUseException:
+    print("Table status:", table.table_status)
+        
+#%%
 
 # Sources
 
